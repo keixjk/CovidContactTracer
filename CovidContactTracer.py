b09eaacd777fb593a6072fd39fe05e0894c9d8d4
keixjk <kayegallogo@gmail.com>
2023-07-30 08:49:53 +0800

    asked user's information

--- CovidContactTracer.py
+++ CovidContactTracer.py
@@ -3,4 +3,9 @@ from tkinter import messagebox
 import json
 
 def add_entry():
-    
+    # Get information from the user
+    name = name_entry.get()
+    phone = phone_entry.get()
+    email = email_entry.get()
+    date = date_entry.get()
+    location = location_entry.get()
